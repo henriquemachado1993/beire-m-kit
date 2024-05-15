e97d96349005d1e272ecb30c58be532311bd5031
henriquemachado1993 <henriquemachado1993@gmail.com>
2024-05-15 20:18:12 -0300

    Added configurationHelper

---
+++ Code/BeireMKit.Domain/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BeireMKit.Domain.Helpers
+{
+    public static class ConfigurationHelper
+    {
+        private static IConfiguration _configuration;
+
+        public static void Initialize(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public static T Get<T>(string key)
+        {
+            return _configuration.GetSection(key).Get<T>();
+        }
+
+        public static T GetChild<T>(string sessionName, string key)
+        {
+            return _configuration.GetSection(sessionName).GetSection(key).Get<T>();
+        }
+    }
+}
