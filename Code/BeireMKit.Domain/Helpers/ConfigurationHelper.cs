d60e6bff31fc3db3ed87b44678e0a0f1f14f9849
henriquemachado1993 <henriquemachado1993@gmail.com>
2024-05-18 13:28:41 -0300

    Add validationConfiguration

--- Code/BeireMKit.Domain/Helpers/ConfigurationHelper.cs
+++ Code/BeireMKit.Domain/Helpers/ConfigurationHelper.cs
@@ -13,12 +13,20 @@ namespace BeireMKit.Domain.Helpers
 
         public static T Get<T>(string key)
         {
+            ValidateConfiguration();
             return _configuration.GetSection(key).Get<T>();
         }
 
         public static T GetChild<T>(string sessionName, string key)
         {
+            ValidateConfiguration();
             return _configuration.GetSection(sessionName).GetSection(key).Get<T>();
+        }
+
+        private static void ValidateConfiguration()
+        {
+            if (_configuration == null)
+                throw new ArgumentNullException(nameof(_configuration), "Configuration has not been initialized. Call Initialize method first.");
         }
     }
 }
