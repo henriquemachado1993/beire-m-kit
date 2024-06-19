652a09fb9eb76fc30591820d1dc83f4606eb4c3c
henriquemachado1993 <henriquemachado1993@gmail.com>
2024-06-19 20:38:13 -0300

    Added querystringextension

---
+++ Code/BeireMKit.Domain/Extensions/QueryStringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Web;
+
+namespace BeireMKit.Domain.Extensions
+{
+    public static class QueryStringExtensions
+    {
+        public static string ToQueryString<T>(this T obj)
+        {
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                      .Where(p => p.GetValue(obj) != null);
+
+            var queryString = string.Join("&", properties.Select(p =>
+            {
+                var value = p.GetValue(obj);
+                return $"{HttpUtility.UrlEncode(p.Name)}={HttpUtility.UrlEncode(value?.ToString())}";
+            }));
+
+            return queryString;
+        }
+    }
+}
