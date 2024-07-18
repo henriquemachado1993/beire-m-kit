a5f0bda773d0fce3c5e488ebf34451c588b455ed
henriquemachado1993 <henriquemachado1993@gmail.com>
2024-07-17 21:39:56 -0300

    Added ToQueryStringWithJsonProperty

--- Code/BeireMKit.Domain/Extensions/QueryStringExtensions.cs
+++ Code/BeireMKit.Domain/Extensions/QueryStringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using Newtonsoft.Json;
+using System.Reflection;
 using System.Web;
 
 namespace BeireMKit.Domain.Extensions
@@ -14,6 +15,22 @@ namespace BeireMKit.Domain.Extensions
             {
                 var value = p.GetValue(obj);
                 return $"{HttpUtility.UrlEncode(p.Name)}={HttpUtility.UrlEncode(value?.ToString())}";
+            }));
+
+            return queryString;
+        }
+
+        public static string ToQueryStringWithJsonProperty<T>(this T obj)
+        {
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                      .Where(p => p.GetValue(obj) != null);
+
+            var queryString = string.Join("&", properties.Select(p =>
+            {
+                var jsonPropAttribute = p.GetCustomAttribute<JsonPropertyAttribute>();
+                var propertyName = jsonPropAttribute != null ? jsonPropAttribute.PropertyName : p.Name;
+                var value = HttpUtility.UrlEncode(p.GetValue(obj)?.ToString());
+                return $"{propertyName}={value}";
             }));
 
             return queryString;
